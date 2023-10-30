fbc842201d6529d16dfedbd787133e1d68610e1b
IvanKurtts <ivankurt96@gmail.com>
2023-10-30 11:16:02 +0100

    cors added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Header, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Access-Control-Allow-Origin', '*')
   getAllCars(
     @Query('limit') limit: string,
     @Query('brand') brand: string,
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://cars-app.vercel.app/',
+    ],
+    methods: ['GET', 'POST'],
+  });
   await app.listen(3002);
 }
 bootstrap();
