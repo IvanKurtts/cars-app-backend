313673cd716227ff7f5bfa78028033a45c277393
IvanKurtts <ivankurt96@gmail.com>
2023-10-30 12:03:54 +0100

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:3002',
       'https://cars-app-ik.vercel.app/',
+      'https://car-app-backend-vf1s.onrender.com',
     ],
     methods: ['GET', 'POST'],
   });
